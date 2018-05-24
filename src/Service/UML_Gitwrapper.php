b479880ec10b48d34051517896f96babb8f17e73
www-data <www-data@44aaf6c1fbe7>
2018-05-24 19:33:15 +0000

    wip testing keys

--- src/Service/UML_Gitwrapper.php
+++ src/Service/UML_Gitwrapper.php
@@ -12,7 +12,7 @@ class UML_Gitwrapper {
 	public function __construct() {
 		//please work
 
-		
+
 		$this->_gitWrapper = new GitWrapper();
 		$this->_gitWrapper->setPrivateKey('/var/www/.ssh/id_rsa');
 		$this->_gitWrapper->git('add src/Service/UML_Gitwrapper.php');
