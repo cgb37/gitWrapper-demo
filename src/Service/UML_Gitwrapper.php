7586e01ea29f7fc96f17d7df8445ccc97a0093e3
www-data <www-data@44aaf6c1fbe7>
2018-05-24 19:16:03 +0000

    wip testing keys

--- src/Service/UML_Gitwrapper.php
+++ src/Service/UML_Gitwrapper.php
@@ -15,7 +15,7 @@ class UML_Gitwrapper {
 		$this->_gitWrapper->setPrivateKey('/www-data/.ssh/id_rsa');
 
 		$this->_gitWrapper->git('add src/Service/UML_Gitwrapper.php');
-		$this->_gitWrapper->git('commit -m "wip testing push"');
+		$this->_gitWrapper->git('commit -m "wip testing keys"');
 		$this->_gitWrapper->git('push');
 
 		// Log to a file named "git.log"
