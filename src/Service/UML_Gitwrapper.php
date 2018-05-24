cb612363ed4433da2a031b3df519ede0a44a736b
www-data <www-data@44aaf6c1fbe7>
2018-05-24 19:35:05 +0000

    wip testing keys

--- src/Service/UML_Gitwrapper.php
+++ src/Service/UML_Gitwrapper.php
@@ -10,7 +10,7 @@ use Monolog\Handler\StreamHandler;
 class UML_Gitwrapper {
 
 	public function __construct() {
-		//please
+		
 		$this->_gitWrapper = new GitWrapper();
 		$this->_gitWrapper->setPrivateKey('/var/www/.ssh/id_rsa');
 		$this->_gitWrapper->git('add src/Service/UML_Gitwrapper.php');
